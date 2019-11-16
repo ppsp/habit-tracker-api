f87d79d61986ad8f25e192b56123674cf4ea3d2c
Pierre-Paul St-Pierre <ppsp5@hotmail.com>
2019-11-16 09:26:46 -0500

    - Added Tools project for infrastructure specific implementations - Added log4net to Tools project - Created Logger static class - Created a log4net config file in debug mode for now - Added a log at the start of the web application

--- HabitTrackerFirebase/Program.cs
+++ HabitTrackerFirebase/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HabitTrackerTools;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -13,6 +14,8 @@ namespace HabitTrackerFirebase
     {
         public static void Main(string[] args)
         {
+            Logger.ConfigLogger();
+            Logger.Debug("Starting HabitTracker");
             CreateHostBuilder(args).Build().Run();
         }
 
---
+++ HabitTrackerTools/Logger.cs
@@ -0,0 +1,70 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace HabitTrackerTools
+{
+    public static class Logger
+    {
+        private static ILog Log = LogManager.GetLogger(Assembly.GetEntryAssembly(), "Logger");
+
+        /// <summary>
+        /// Configure the logger and it will depend of the config file in : Application.StartupPath + @"\log4netConfig.xml"
+        /// </summary>
+        static public void ConfigLogger()
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+        }
+
+        static public void Debug(object message)
+        {
+            if (Log != null)
+                Log.Debug(message);
+        }
+
+        static public void Debug(object message, Exception exception)
+        {
+            if (Log != null)
+                Log.Debug(message, exception);
+        }
+
+        static public void Warn(object message)
+        {
+            if (Log != null)
+                Log.Warn(message);
+        }
+
+        static public void Warn(object message, Exception exception)
+        {
+            if (Log != null)
+                Log.Debug(message, exception);
+        }
+
+        static public void Info(object message)
+        {
+            if (Log != null)
+                Log.Info(message);
+        }
+
+        static public void Info(object message, Exception exception)
+        {
+            if (Log != null)
+                Log.Info(message, exception);
+        }
+
+        static public void Error(object message)
+        {
+            if (Log != null)
+                Log.Error(message);
+        }
+
+        static public void Error(object message, Exception exception)
+        {
+            if (Log != null)
+                Log.Error(message, exception);
+        }
+    }
+}
