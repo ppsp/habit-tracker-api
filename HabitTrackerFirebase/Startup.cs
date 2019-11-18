fc5bfbb25bb5ed580863edfff5a173c6811ab950
Pierre-Paul St-Pierre <ppsp5@hotmail.com>
2019-11-18 12:11:14 -0500

    - Stop converting returned DTO's to camelCase (leave them in PascalCase)

--- HabitTrackerFirebase/Startup.cs
+++ HabitTrackerFirebase/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace HabitTrackerFirebase
 {
@@ -25,7 +19,11 @@ namespace HabitTrackerFirebase
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+                    .AddNewtonsoftJson(options =>
+                    {
+                        options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                    });
             services.AddApplicationInsightsTelemetry();
         }
 
