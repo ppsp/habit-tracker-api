aa4a613150891469b3b623a11a4ba6779f0419e5
Pierre-Paul St-Pierre <ppsp5@hotmail.com>
2019-11-25 17:54:47 -0500

    - Forgot to stage these changes in previous commit

---
+++ HabitTrackerTools/AzureVaultConnector.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+
+namespace HabitTrackerTools
+{
+    public class AzureVaultConnector
+    {
+        private string VaultName { get; set; }
+
+        public AzureVaultConnector(string vaultName)
+        {
+            this.VaultName = vaultName;
+        }
+
+        public string GetSecretValueString(string key)
+        {
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+            KeyVaultClient keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+            var secretJson = keyVaultClient.GetSecretAsync($"https://{this.VaultName}.vault.azure.net/secrets/{key}")
+                                           .ConfigureAwait(false).GetAwaiter().GetResult();
+
+            return secretJson.Value;
+        }
+    }
+}
