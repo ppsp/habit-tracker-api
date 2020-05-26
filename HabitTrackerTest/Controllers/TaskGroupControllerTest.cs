0d0ba4e33f366089a352e88eb2116f7856153212
Pierre-Paul St-Pierre <ppsp5@hotmail.com>
2020-05-25 22:08:50 -0400

    - Added tests for controller

--- HabitTrackerTest/Controllers/TaskGroupControllerTest.cs
+++ HabitTrackerTest/Controllers/TaskGroupControllerTest.cs
@@ -52,8 +52,38 @@ namespace HabitTrackerTest
             var okResult = response as OkObjectResult;
 
             // ASSERT
-            Assert.IsTrue(okResult.Value is DTOTaskGroup);
-            // Assert.IsTrue((okResult.Value as DTOTaskGroup).Length > 0);
+            Assert.IsTrue(okResult.Value is List<DTOTaskGroup>);
+            var dtoTaskGroup = okResult.Value as List<DTOTaskGroup>;
+            Assert.IsTrue(AssertValuesAreTheSame(testGroup, dtoTaskGroup[0].ToTaskGroup()));
+            Assert.AreEqual(200, okResult.StatusCode);
+
+            DeleteAllGroups();
+        }
+
+        [TestMethod]
+        public void TaskGroup_Put_ShouldReturnTrue()
+        {
+            DeleteAllGroups();
+
+            // ARRANGE
+            var testGroup = getTestTaskGroup();
+            testGroup.Id = this.taskGroupService.InsertGroupAsync(testGroup).Result;
+
+            // var retrievedGroup = this.taskGroupService.GetGroupAsync(testGroup.GroupId).Result;
+
+            // ACT
+            var updatedGroup = this.taskGroupService.GetGroupAsync(testGroup.GroupId).Result;
+            updatedGroup.GroupName = "NewName2";
+            updatedGroup.GroupPosition = 32;
+            updatedGroup.Void = true;
+            updatedGroup.ColorHex = "ABBABC";
+
+            var response = taskGroupController.Put(DTOTaskGroup.FromTaskGroup(updatedGroup)).Result;
+            var okResult = response as OkObjectResult;
+
+            // ASSERT
+            Assert.IsTrue(okResult.Value is bool);
+            Assert.IsTrue((bool)okResult.Value);
             Assert.AreEqual(200, okResult.StatusCode);
 
             DeleteAllGroups();
